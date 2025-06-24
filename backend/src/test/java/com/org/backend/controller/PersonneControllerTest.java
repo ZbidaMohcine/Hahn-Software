66c51575202f6e80b7b91e2bc82b249e39131a80
m.zbida <mohcine.zbida@seven-app.net>
2025-06-24 15:14:55 +0100

    package the apps using docker compose

--- backend/src/test/java/com/org/backend/controller/PersonneControllerTest.java
+++ backend/src/test/java/com/org/backend/controller/PersonneControllerTest.java
@@ -10,8 +10,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Collections;
 
@@ -25,7 +25,7 @@ public class PersonneControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private PersonneService personneService;
 
     @Autowired
